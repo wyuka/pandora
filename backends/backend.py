159baf840155968aedbf452cd324f13c5c13861b
Tirtha Chatterjee <tirtha.p.chatterjee@gmail.com>
2011-05-08 17:03:35 +0530

    added dummy backends

---
+++ backends/backend.py
@@ -0,0 +1,13 @@
+import proxy
+
+class Backend(proxy.proxyable):
+    
+    def __init__():
+        proxy.proxyable.__init__(self)
+        
+    def pullChanges():
+        pass
+    
+    def pushChanges():
+        pass
+    
